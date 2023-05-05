f327d295bc393a01c1de787b28a0f7add423b854
AdamHrioui1 <74858612+AdamHrioui1@users.noreply.github.com>
2023-05-05 19:04:19 +0100

    Add files via upload
    make the code more readable

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ let two_selected_cards = []
 let score = 0
 let matched_cards_counter = 0
 let cards_wrapper = document.getElementById('cards_wrapper')
-let size;
+let layout_size;
 
 let chooseOne = () => {
     let btn_4x4 = document.getElementById('btn_4x4')
     let btn_6x6 = document.getElementById('btn_6x6')
 
     btn_4x4.addEventListener('click', () => {
-        size = 4
+        layout_size = 4
         displayCards(shuffleArray(imgs_4x4), 4)
     })
     btn_6x6.addEventListener('click', () => {
-        size = 6
+        layout_size = 6
         displayCards(shuffleArray(imgs_6x6), 6)
     })
 }
@@ -26,45 +26,7 @@ function shuffleArray(array) {
     return array;
 }
 
-
-let selectCard = (e) => {
-    showScore(score)
-    let id = parseInt(e.currentTarget.id)
-    
-    score++;
-    two_selected_cards.push(id)
-    
-    let selectedCard = document.getElementById(id)
-    selectedCard.classList.add('flipped')
-
-    if(score % 2 === 0) {
-        if(isCardsMatches(two_selected_cards[0], two_selected_cards[1], size)) {
-            matched_cards_counter++;
-            console.log(matched_cards_counter)
-            two_selected_cards.map((card) => {
-                let selectedCard = document.getElementById(card)
-                selectedCard.classList.add('flipped')
-                selectedCard.removeEventListener('click', selectCard, true)
-            })
-            two_selected_cards = []
-        }
-        else {
-            setTimeout(() => {
-                two_selected_cards.map((card) => {
-                    let selectedCard = document.getElementById(card)
-                    selectedCard.classList.remove('flipped')
-                })
-                two_selected_cards = []
-
-                return 0
-            }, 1000)
-        }
-    }
-
-    youWin()
-}
-
-let displayCards = (shuffled_array, size) => {
+let displayCards = (shuffled_array, layout_size) => {
     document.getElementById('btns_wrapper').style.display = 'none'
 
     for (let i = 0; i < shuffled_array.length; i++) {
@@ -72,8 +34,8 @@ let displayCards = (shuffled_array, size) => {
         card.setAttribute('class', 'card')
         card.setAttribute('id', shuffled_array[i].id)
 
-        if(size === 4) card.classList.add('four')
-        if(size === 6) card.classList.add('six')
+        if(layout_size === 4) card.classList.add('four')
+        if(layout_size === 6) card.classList.add('six')
 
         card.innerHTML = `
             <div class="front">
@@ -99,9 +61,62 @@ let flipAllCards = () => {
         cards.forEach((c) => {
             c.classList.remove('flipped')
         })
-
         timer()
     }, 3000)
+}
+
+let selectCard = (e) => {
+    score++;
+    showScore(score)
+    
+    let id = parseInt(e.currentTarget.id)
+    two_selected_cards.push(id)
+    
+    let selectedCard = document.getElementById(id)
+    selectedCard.classList.add('flipped')
+
+    if(score % 2 === 0) {
+        if(isCardsMatches(two_selected_cards[0], two_selected_cards[1], layout_size)) {
+            matched_cards_counter++;
+            two_selected_cards.map((card) => {
+                let selectedCard = document.getElementById(card)
+                selectedCard.classList.add('flipped')
+                selectedCard.removeEventListener('click', selectCard, true)
+            })
+            two_selected_cards = []
+        }
+        else {
+            setTimeout(() => {
+                two_selected_cards.map((card) => {
+                    let selectedCard = document.getElementById(card)
+                    selectedCard.classList.remove('flipped')
+                })
+                two_selected_cards = []
+
+                return 0
+            }, 1000)
+        }
+    }
+
+    youWin()
+}
+
+let isCardsMatches = (card1, card2, layout_size) => {
+    if(layout_size === 4) return matches_4x4[card1] === card2 
+    else return matches_6x6[card1] === card2 
+}
+
+let youWin = (time) => {
+    if(matched_cards_counter === layout_size**2 / 2) {
+        let container = document.getElementById('container')
+        container.innerHTML = `
+            <h1>You win!</h1>
+            <h1>Score: ${score}</h1>
+            <h1>Time: ${time} sec</h1>
+        `
+        return true
+    }
+    return false;
 }
 
 let timer = () => {
@@ -115,26 +130,8 @@ let timer = () => {
     }, 1000);
 }
 
-let showScore = (score) => document.getElementById('score').innerText = 'Score: ' + score
-
 let showTimer = (time) => document.getElementById('time').innerText = 'Time: ' + time+ ' sec'
 
-let youWin = (time) => {
-    if(matched_cards_counter === size**2 / 2) {
-        let container = document.getElementById('container')
-        container.innerHTML = `
-            <h1>You win!</h1>
-            <h1>Score: ${score}</h1>
-            <h1>Time: ${time} ec</h1>
-        `
-        return true
-    }
-    return false;
-}
-
-let isCardsMatches = (card1, card2, size) => {
-    if(size === 4) return matches_4x4[card1] === card2 
-    else return matches_6x6[card1] === card2 
-}
+let showScore = (score) => document.getElementById('score').innerText = 'Score: ' + score
 
 chooseOne()
